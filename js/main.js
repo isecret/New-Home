5c1a1d0ae8c6f7a5cb92c455192afd2549373b6b
isecret <572524331@qq.com>
2017-06-01 21:13:10 +0800

    删除大图，调用我博客的大图

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $.post("https://sslapi.hitokoto.cn/",null,function(e){
 },'JSON');
 // 背景大图加载
 var img = new Image();
-img.src = "./images/bg.jpg";
+img.src = "http://isecret.vip/images/bg.jpg";
 $(img).load(function(){
 	$('#bg').css("backgroundImage","url("+img.src+")");
 });
