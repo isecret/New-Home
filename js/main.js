920dcb9090f1a42bb336adb5b7fd08ec509a0dc2
isecret <572524331@qq.com>
2017-06-01 20:51:50 +0800

    上传所有

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// 一言调用
+$.post("https://sslapi.hitokoto.cn/",null,function(e){
+	$('.content').html(e.hitokoto+" —— <strong>"+e.from+"</strong>")
+},'JSON');
+// 背景大图加载
+var img = new Image();
+img.src = "./images/bg.jpg";
+$(img).load(function(){
+	$('#bg').css("backgroundImage","url("+img.src+")");
+});
